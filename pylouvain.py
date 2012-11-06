39ab856b1dc17305c9d32636f9f3b87f78b273a1
patapizza <patapizza@gmail.com>
2012-11-06 14:39:36 +0100

    implementing first phase

--- pylouvain.py
+++ pylouvain.py
@@ -28,11 +28,12 @@ class PyLouvain:
         Applies the Louvain method.
     '''
     def apply_method(self):
-        partition = self.make_initial_partition()
-        best_partition = partition
+        network = (self.nodes, self.edges)
+        best_partition = self.make_initial_partition()
         while 1:
-            partition = self.first_phase(partition)
-            partition = self.second_phase(partition)
+            # TODO: precompute parameter m (and k vector?)
+            partition = self.first_phase(network)
+            nodes, edges = self.second_phase(network, partition)
             if partition == best_partition:
                 break
             best_partition = partition
@@ -41,35 +42,98 @@ class PyLouvain:
     '''
         Computes the modularity of _network.
         _network: a (nodes, edges) pair
+        _partition: a list of lists of nodes
     '''
-    def compute_modularity(self, network):
-        pass
+    def compute_modularity(self, network, partition):
+        # compute m
+        m = 0
+        for e in network[1]:
+            m += e[1]
+        q = 0
+        for i in network[0]:
+            for j in network[0]:
+                continue if self.get_community(i, partition) == self.get_community(j, partition)
+                q += self.get_weight(i, j, network[1]) - (self.compute_weights(i, network[1]) * self.compute_weights(j, network[1]) / m)
+        return q / m
+
+    '''
+        Computes the sum of the weights of the edges attached to vertex _i.
+        _i: a node
+        _edges: a list of ((node, node), weight) pairs
+    '''
+    def compute_weights(self, i, edges):
+        w = 0
+        for e in edges:
+            if e[0][0] == i:
+                w += e[1]
+        return w
 
     '''
         Performs the first phase of the method.
+        _network: a (nodes, edges) pair
+    '''
+    def first_phase(self, network):
+        partition = self.make_initial_partition(network[0])
+        best_partition = (self.compute_modularity(network, partition), partition)
+        while 1:
+            best_modularity = best_partition[0]
+            for node in network[0]:
+                for neighbor in self.get_neighbors(node, network[1]):
+                    # move _node from its community to the community of _neighbor
+                    current_partition = [[pp for pp in p] for p in partition]
+                    current_partition[self.get_community(node, partition)].remove(node)
+                    current_partition[self.get_community(neighbor, partition)].append(node)
+                    # compute modularity obtained
+                    current_modularity = self.compute_modularity(network, current_partition)
+                    best_partition = (current_modularity, current_partition) if best_partition[0] < current_modularity
+                # move node from its community to the one of the neighbor maximizing the modularity gain (if positive)
+                partition = best_partition[1]
+            break if best_partition[0] == best_modularity: # no improvement
+        return partition
+
+    '''
+        Returns the community in which _node is (among _partition).
+        _node: an int
         _partition: a list of lists of nodes
     '''
-    def first_phase(self, partition):
-        best_modularity = 0
-        while 1:
-            for node in nodes:
-                for neighbor in self.get_neighbors(node):
-                    # move node from its partition to the partition of neighbor
-                    # compute modularity obtained
-                # move node from its partition to the one of the neighbor maximizing the modularity gain (if positive)
-            if current_modularity == best_modularity: # no improvement
-                break
+    def get_community(self, node, partition):
+        for i in range(len(partition)):
+            if node in partition[i]:
+                return i
+        return -1
+
+    '''
+        Yields the nodes adjacent to _node.
+        _node: an int
+        _edges: a list of ((node, node), weight) pairs
+    '''
+    def get_neighbors(self, node, edges):
+        yield e for e in edges if e[0][0] == node or e[0][1] == node
+
+    '''
+        Returns the weight of edge (_i, _j) among _edges.
+        _i: a node
+        _j: a node
+        _edges: a list of ((node, node), weight) pairs
+    '''
+    def get_weight(self, i, j, edges):
+        for e in edges:
+            if e[0][0] == i and e[0][1] == j:
+                return e[1]
+        return -1
 
     '''
         Builds the initial partition.
+        _nodes: list of ints
     '''
-    def make_initial_partition(self):
-        return [[node] for node in self.nodes]
+    def make_initial_partition(self, nodes):
+        return [[node] for node in nodes]
 
     '''
         Performs the second phase of the method.
+        _network: a (nodes, edges) pair
         _partition: a list of lists of nodes
     '''
-    def second_phase(self, partition):
+    def second_phase(self, network, partition):
         pass
 
