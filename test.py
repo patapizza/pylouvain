865323612e2e1217c20c7ffa15a12c225f4732c2
patapizza <patapizza@gmail.com>
2012-11-20 17:02:40 +0100

    pushing some samples into data directory for testing purposes

--- test.py
+++ test.py
@@ -7,15 +7,20 @@ from pylouvain import PyLouvain
 class PylouvainTest(unittest.TestCase):
 
     def test_karate_club(self):
-        pyl = PyLouvain.from_file("karate.txt")
+        pyl = PyLouvain.from_file("data/karate.txt")
         partition, q = pyl.apply_method()
         q_ = q * 10000
         self.assertEqual(4, len(partition))
         self.assertEqual(4298, math.floor(q_))
         self.assertEqual(4299, math.ceil(q_))
 
+    @unittest.skip("takes up to 242 sec")
     def test_arxiv(self):
-        pyl = PyLouvain.from_file("hep-th-citations")
+        pyl = PyLouvain.from_file("data/hep-th-citations")
+        partition, q = pyl.apply_method()
+
+    def test_lesmis(self):
+        pyl = PyLouvain.from_gml_file("data/lesmis.gml")
         partition, q = pyl.apply_method()
 
 if __name__ == '__main__':
