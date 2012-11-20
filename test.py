07f3604d09082cfab107f5da796707acfa38b579
patapizza <patapizza@gmail.com>
2012-11-20 17:56:32 +0100

    adding sample datasets

--- test.py
+++ test.py
@@ -6,6 +6,14 @@ from pylouvain import PyLouvain
 
 class PylouvainTest(unittest.TestCase):
 
+    def test_arxiv(self):
+        pyl = PyLouvain.from_file("data/arxiv.txt")
+        partition, q = pyl.apply_method()
+
+    def test_citations(self):
+        pyl = PyLouvain.from_file("data/hep-th-citations")
+        partition, q = pyl.apply_method()
+
     def test_karate_club(self):
         pyl = PyLouvain.from_file("data/karate.txt")
         partition, q = pyl.apply_method()
@@ -14,13 +22,12 @@ class PylouvainTest(unittest.TestCase):
         self.assertEqual(4298, math.floor(q_))
         self.assertEqual(4299, math.ceil(q_))
 
-    @unittest.skip("takes up to 242 sec")
-    def test_arxiv(self):
-        pyl = PyLouvain.from_file("data/hep-th-citations")
-        partition, q = pyl.apply_method()
-
     def test_lesmis(self):
         pyl = PyLouvain.from_gml_file("data/lesmis.gml")
+        partition, q = pyl.apply_method()
+
+    def test_polbooks(self):
+        pyl = PyLouvain.from_gml_file("data/polbooks.gml")
         partition, q = pyl.apply_method()
 
 if __name__ == '__main__':
